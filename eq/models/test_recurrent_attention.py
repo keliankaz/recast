da6071a2d8b8d682e946a587648e56a5f358152a
Zekai Wang <zekai.wang@berkeley.edu>
2023-10-05 13:00:29 -0700

    debugging recast_attention

--- eq/models/test_recurrent_attention.py
+++ eq/models/test_recurrent_attention.py
@@ -11,16 +11,18 @@ t_end = 20.0
 mag = np.array([4.0, 2.1, 2.5, 2.9])
 inter_times = np.diff(arrival_times, prepend=[t_start], append=[t_end])
 seq = eq.data.Sequence(inter_times, t_start=0.0, mag=mag)
-
-catalog = eq.catalogs.White()
+#%%
+catalog = eq.catalogs.White(mag_completeness=4)
 batch_list = []
-for _ in range(10):
-    batch = next(iter(catalog.train.get_dataloader())) # eq.data.Batch.from_list([seq])
-    batch_list.append(batch)
+for _ in range(3):
+    seq = catalog.train[0] # eq.data.Batch.from_list([seq])
+    batch_list.append(seq)
 batch_multiple = eq.data.Batch.from_list(batch_list)
-print("NO ERROR")
+
 model = RecurrentTPP_Attention()
 context = model.get_context(batch_multiple)
 distr = model.get_inter_time_dist(context)
+log_pdf = distr.log_prob(batch_multiple.inter_times.clamp_min(1e-10))
+print("NO ERROR")
 print("finish")
 # %%
