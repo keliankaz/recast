f0243d33126380430775d77ec3d415f44f017519
Jonah_Koepke <jonahk@berkeley.edu>
2023-10-26 10:26:41 -0700

    test

--- eq/catalogs/anss.py
+++ eq/catalogs/anss.py
@@ -153,7 +153,7 @@ class ANSS_MultiCatalog(Catalog):
                     inter_times=torch.as_tensor(inter_times, dtype=torch.float32),
                     t_start=t_start,
                     mag=mag,
-                    extra_feat = torch.as_tensor(depth, dtype=torch.float32).unsqueeze(-1)
+                    extra_feat = torch.as_tensor(depth, dtype=torch.float32)
                 )
             )
 
