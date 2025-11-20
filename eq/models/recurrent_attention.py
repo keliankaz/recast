d08c213067ddfb7fbeb183757cac22455a3b18d7
keliankaz <kdascher@ucsc.edu>
2025-11-20 15:01:37 -0700

    moved tests to only run in main because I am not currently maintaining recurrent_attention

--- eq/models/recurrent_attention.py
+++ eq/models/recurrent_attention.py
@@ -1,13 +1,11 @@
-from typing import List, Optional, Tuple, Union
+from typing import Optional
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Categorical
 
 import eq
-import eq.distributions as dist
-
+import numpy as np
 from eq.models.recurrent import RecurrentTPP
 
 
@@ -82,3 +80,26 @@ class RecurrentTPP_Attention(RecurrentTPP):
         truncated_attention_output = attention_output[:, :-1, :]
         output = F.pad(truncated_attention_output, (0, 0, 1, 0))  # (B, L, C)
         return self.dropout(output)  # (B, L, C)
+    
+if __name__ == "__main__":
+    t_start = 10.0
+    arrival_times = np.array([11.1, 11.5, 12.1, 14.2])
+    # If you don't know t_end, just set t_end = arrival_times[-1]
+    t_end = 20.0
+    mag = np.array([4.0, 2.1, 2.5, 2.9])
+    inter_times = np.diff(arrival_times, prepend=[t_start], append=[t_end])
+    seq = eq.data.Sequence(inter_times, t_start=0.0, mag=mag)
+    #%%
+    catalog = eq.catalogs.White(mag_completeness=4)
+    batch_list = []
+    for _ in range(3):
+        seq = catalog.train[0] # eq.data.Batch.from_list([seq])
+        batch_list.append(seq)
+    batch_multiple = eq.data.Batch.from_list(batch_list)
+
+    model = RecurrentTPP_Attention()
+    context = model.get_context(batch_multiple)
+    distr = model.get_inter_time_dist(context)
+    log_pdf = distr.log_prob(batch_multiple.inter_times.clamp_min(1e-10))
+    print("NO ERROR")
+    print("finish")
